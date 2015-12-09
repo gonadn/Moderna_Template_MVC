50d86503c13719a0de549dc277047b2efb606719
Adnan Riaz Gondal <gonadn@live.no>
2015-12-09 13:35:27 +0100

    Comitting Moderna

---
+++ Moderna/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Moderna
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/appjs").Include(
+                     "~/js/jquery-1.10.2",
+                     "~/js/validate.js",
+                     "~/js/jquery.easing.1.3.js",
+                     "~/js/bootstrap.min.js",
+                     "~/js/jquery.fancybox.pack.js",
+                     "~/js/jquery.fancybox-media.js",
+                     "~/js/google-code-prettify/prettify.js",
+                     "~/js/portfolio/jquery.quicksand.js",
+                     "~/js/portfolio/setting.js",
+                     "~/js/jquery.flexslider.js",
+                     "~/js/animate.js",
+                     "~/js/custom.js",
+                     "~/js/facebook.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/fancybox/jquery.fancybox.css",
+                      "~/css/jcarousel.css",
+                      "~/css/flexslider.css",
+                      "~/css/style.css",
+                      "~/skins/default.css"));
+        }
+    }
+}
---
+++ Moderna/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Moderna.MvcApplication" Language="C#" %>
